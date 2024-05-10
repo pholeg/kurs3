3eb9d7e73523183ce6fc3383e096e2a4ea2f312f
ph_oleg <ph_oleg@mail.ru>
2024-05-10 20:29:51 +0300

    чуток поправил код и добавил тесты pytest

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ from utils import formate_date
 from utils import un_visible_number
 
 
-for key_, value_ in last_five_transactions().items():
-    for trans in load_transactions():
-        if not trans == {} and trans['id'] == value_:
-            print(f"{formate_date(trans['date'])} {trans['description']}")
-            if 'from' in trans.keys(): print(f"{un_visible_number(trans['from'])} -> ", end='')
-            if 'to' in trans.keys(): print(f"{un_visible_number(trans['to'])}")
-            print(f"{trans['operationAmount']['amount']} {trans['operationAmount']['currency']['name']}\n")
+def main():
+    """Формируем вывод по заданному шаблону. На входе dict из исходной выписки и dict c 5ю последними ID"""
+    for key_, value_ in last_five_transactions().items():
+        for trans in load_transactions():
+            if not trans == {} and trans['id'] == value_:
+                print(f"{formate_date(trans['date'])} {trans['description']}")
+                if 'from' in trans.keys():
+                    print(f"{un_visible_number(trans['from'])} -> ", end='')
+                if 'to' in trans.keys():
+                    print(f"{un_visible_number(trans['to'])}")
+                print(f"{trans['operationAmount']['amount']} {trans['operationAmount']['currency']['name']}\n")
+
+
+main()
